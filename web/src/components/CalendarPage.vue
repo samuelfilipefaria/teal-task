f93beb0786983ebee81958e7d733801f892fade9
Samuel Filipe Faria <samuelfilipefaria@gmail.com>
2023-12-13 19:11:27 -0300

    Implementing dialog on calendar

--- web/src/components/CalendarPage.vue
+++ web/src/components/CalendarPage.vue
@@ -1,6 +1,16 @@
 <template lang="pug">
 h1(style="text-align: center;") Calendar
-el-calendar(ref="mycal" v-model="selectedDate")
+el-calendar(ref="mycal" v-model="selectedDate" @click="showTasks")
+el-dialog(
+  v-model="dialogVisible"
+  :title="dialogTitle"
+  width="30%"
+)
+  ul
+    li Terminar trabalho faculdade
+    li Limpar quarto
+    li Fazer exercícios
+    li Pegar carro no mecânico
 </template>
 
 <script>
@@ -9,15 +19,21 @@ export default {
   data() {
     return {
       selectedDate: new Date(),
+      dialogVisible: false,
     };
   },
-  wacth: {
-    selectedDate() {
-      console.log(`mudou`);
+  computed: {
+    dialogTitle() {
+      return `Tarefas agendadas para ${this.formattedSelectedDate}`;
+    },
+    formattedSelectedDate() {
+      return `${this.selectedDate.getDate()}/${this.selectedDate.getMonth()}/${this.selectedDate.getFullYear()}`;
     },
   },
-  mounted() {
-    console.log(this.$refs.mycal.value);
+  methods: {
+    showTasks() {
+      this.dialogVisible = true;
+    },
   },
 };
 </script>
