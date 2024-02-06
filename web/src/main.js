e6d64657b9e7fbe0f73a3b5b328bde6911b87768
Samuel Filipe Faria <samuelfilipefaria@gmail.com>
2024-02-06 15:16:42 -0300

    Revert "Revert "Installing dependencies to make the kanban""
    This reverts commit cc2d44955e6ce4cff56e087058040e4b6086f742.

--- web/src/main.js
+++ web/src/main.js
@@ -5,8 +5,10 @@ import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import "./assets/css/global.css";
 import router from "./router";
+import vueKanban from "vue-kanban";
 
 const app = createApp(App);
 app.use(ElementPlus);
 app.use(router);
+app.use(vueKanban);
 app.mount("#app");
