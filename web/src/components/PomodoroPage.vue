b8f048cb433f15c24d469d7fec45a858b3aa053f
Samuel Filipe Faria <samuelfilipefaria@gmail.com>
2024-02-12 15:43:49 -0300

    Fixing pomodoro timer

--- web/src/components/PomodoroPage.vue
+++ web/src/components/PomodoroPage.vue
@@ -2,7 +2,8 @@
 h1(style="text-align: center;") Pomodoro
 div(style="text-align: center;")
   el-progress(type="circle" :percentage="counterPercentage" color="#008080")
-    box-icon(type="solid" name="hourglass")
+    h2(v-if="isCounting") {{ formattedTimer }}
+    box-icon(v-else type="solid" name="hourglass")
   br
   h2 {{ displayText }}
   br
@@ -15,8 +16,9 @@ export default {
   data() {
     return {
       isCounting: false,
-      seconds: 11,
+      seconds: 1500,
       isBreak: false,
+      intervalId: null,
       displayText:
         "Inicie a contagem quando estiver preparado para começar um ciclo pomodoro",
     };
@@ -29,35 +31,45 @@ export default {
       return this.isCounting ? this.stopPomdoro : this.startPomodoro;
     },
     counterPercentage() {
-      let total = this.isBreak ? 6 : 11;
+      let total = this.isBreak ? 300 : 1500;
       return (this.seconds * 100) / total;
+    },
+    formattedTimer() {
+      return `
+        ${Math.floor(this.seconds / 60)} : ${
+        this.seconds - Math.floor(this.seconds / 60) * 60
+      }
+      `;
     },
   },
   methods: {
     startPomodoro() {
+      this.isBreak = false;
       this.isCounting = true;
       this.displayText =
         "Pode se concentrar tranquilo, nós te avisaremos quando os 25 minutos acabarem";
-      setInterval(this.decreaseOneSecond, 1000);
+      this.intervalId = setInterval(this.decreaseOneSecond, 1000);
     },
     stopPomdoro() {
+      this.isBreak = false;
       this.isCounting = false;
-      this.seconds = 11;
+      this.seconds = 1500;
       this.displayText =
         "Inicie a contagem quando estiver preparado para começar um ciclo pomodoro";
+      clearInterval(this.intervalId);
     },
     decreaseOneSecond() {
       if (!this.isCounting) return;
 
       if (this.seconds == 0 && !this.isBreak) {
         this.isBreak = true;
-        this.seconds = 6;
+        this.seconds = 300;
         this.playNotification();
         this.displayText =
           "Você já se concentrou, hora de relaxar e descansar um pouquinho, te avisaremos quando for para voltar";
       } else if (this.seconds == 0 && this.isBreak) {
         this.isBreak = false;
-        this.seconds = 11;
+        this.seconds = 1500;
         this.playNotification();
         this.displayText =
           "Pode se concentrar tranquilo, nós te avisaremos quando os 25 minutos acabarem";
