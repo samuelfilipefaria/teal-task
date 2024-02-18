98de65be5471dc1a6906a14a7eadeb81a4fc4090
Samuel Filipe Faria <samuelfilipefaria@gmail.com>
2024-02-18 19:46:46 -0300

    Creating routes and controller for kanban cards

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :tags, only: [:index, :show, :create, :update, :destroy]
   resources :kanban_columns, only: [:index, :show, :create, :update, :destroy]
+  resources :kanban_cards, only: [:index, :show, :create, :update, :destroy]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
