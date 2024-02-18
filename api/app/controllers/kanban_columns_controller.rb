3a0ed6702ffd42c955164b3e9558177490120921
Samuel Filipe Faria <samuelfilipefaria@gmail.com>
2024-02-18 03:16:30 -0300

    Creating methods to manage kanban columns

--- api/app/controllers/kanban_columns_controller.rb
+++ api/app/controllers/kanban_columns_controller.rb
@@ -1,16 +1,56 @@
 class KanbanColumnsController < ApplicationController
   def index
+    columns = KanbanColumn.all
+    render json: columns, status: 200
   end
 
   def show
+    column = KanbanColumn.find(params[:id])
+
+    if column
+      render json: column, status: 200
+    else
+      render json: {error: "Coluna não encontrada!"}
+    end
   end
 
   def create
+    new_column = KanbanColumn.new(
+      title: params[:title],
+      color: params[:color],
+      position: params[:position]
+    )
+
+    if new_column.save
+      render json: new_column, status: 200
+    else
+      render json: {error: "Erro ao criar coluna!"}
+    end
   end
 
   def update
+    column = KanbanColumn.find(params[:id])
+
+    unless column
+      render json: {error: "Coluna não encontrada!"}
+      return
+    end
+
+    column.update(
+      title: params[:title],
+      color: params[:color],
+      position: params[:position]
+    )
   end
 
   def destroy
+    column = KanbanColumn.find(params[:id])
+
+    unless column
+      render json: {error: "Coluna não encontrada!"}
+      return
+    end
+
+    column.destroy
   end
 end
