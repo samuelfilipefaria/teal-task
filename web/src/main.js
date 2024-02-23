55592498ba96cf82d8190365a472cdca1a53d5dc
Samuel Filipe Faria <samuelfilipefaria@gmail.com>
2024-02-23 11:42:22 -0300

    Adding animate.css

--- web/src/main.js
+++ web/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "boxicons";
+import "animate.css";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import "./assets/css/global.css";
